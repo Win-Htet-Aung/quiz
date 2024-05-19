dfe172af989d4a73788fc6554ed43d496f6fbde2
Win Htet Aung <whaung1998@gmail.com>
2024-05-19 16:23:50 +0700

    user can select question to add to quiz

--- src/main/java/com/example/repository/QuestionRepository.java
+++ src/main/java/com/example/repository/QuestionRepository.java
@@ -1,9 +1,13 @@
 package com.example.repository;
 
+import java.util.List;
+
 import org.hibernate.Session;
 import org.hibernate.Transaction;
+import org.hibernate.query.Query;
 
 import com.example.entity.Question;
+import com.example.entity.Quiz;
 
 public class QuestionRepository extends Repository {
     public void CreateQuestion(Question question) {
@@ -13,5 +17,23 @@ public class QuestionRepository extends Repository {
         session.persist(question);
         transaction.commit();
         session.close();
+    }
+
+    public List<Question> GetAllQuestions() {
+        Session session = sessionFactory.openSession();
+        Query<Question> q = session.createQuery("select q from Question q", Question.class);
+        List<Question> result = q.getResultList();
+        session.close();
+        return result;
+    }
+
+    public Question GetQuestionById(Long qid) {
+        Question question = null;
+        Session session = sessionFactory.openSession();
+        Query<Question> q = session.createQuery("select q from Question q where q.id = :qid", Question.class);
+        q.setParameter("qid", qid);
+        question = q.getSingleResult();
+        session.close();
+        return question;
     }
 }
--- src/main/java/com/example/services/QuestionService.java
+++ src/main/java/com/example/services/QuestionService.java
@@ -1,6 +1,9 @@
 package com.example.services;
 
+import java.util.List;
+
 import com.example.entity.Question;
+import com.example.entity.Quiz;
 import com.example.utils.Context;
 
 public class QuestionService {
@@ -18,5 +21,17 @@ public class QuestionService {
         String c3 = ctx.getSc().nextLine();
         Question question = new Question(question_text, c1, c2, c3, answer);
         ctx.getQuestionRepo().CreateQuestion(question);
+        System.out.print("\nDo you want to add this question to a quiz?(y/n) : ");
+    }
+
+    public static Question GetQuestionById(Context ctx, Long qid) {
+        return ctx.getQuestionRepo().GetQuestionById(qid);
+    }
+
+    public static void ShowQuestions(Context ctx) {
+        List<Question> questions = ctx.getQuestionRepo().GetAllQuestions();
+        for (Question q: questions) {
+            System.out.println(String.format("%d  %s", q.getId(), q.getQuestion()));
+        }
     }
 }
